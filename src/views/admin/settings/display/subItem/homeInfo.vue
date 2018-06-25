0412fa374ec7564bec77adbadb0952cfd04c1e0d
Liu Runhua <2030196696@qq.com>
2018-06-25 15:32:24 +0800

    使用帮助上传

--- src/views/admin/settings/display/subItem/homeInfo.vue
+++ src/views/admin/settings/display/subItem/homeInfo.vue
@@ -1,74 +1,53 @@
 <style lang="less">
+   @import "./homeInfo.less";
 </style>
 <template>
-    <div class="homeInfo">
-        <span>空间类型&nbsp;&nbsp;</span>
-        <Select v-model="currentType" style="width:200px" @on-change='handleSelectChange'>
-            <Option v-for="item in spaceList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
+    <div class="admin-homeInfo">
         <Row :gutter="10">
             <Col span="12">
-                <Form ref="formCustom" :model="form" :label-width="80">
-                    <Card>
-                        <FormItem label="空间简介" prop="introduce">
-                            <Input type="textarea" v-model="form.introduce"></Input>
-                        </FormItem>
-                    </Card>
-                    <Card>
-                        <h2>预约须知</h2>
-                        <FormItem label="标题" prop="notice.title">
-                            <Input type="text" v-model="form.notice.title"></Input>
-                        </FormItem>
-                        <FormItem label="前言" prop="notice.prolusion">
-                            <Input type="textarea" v-model="form.notice.prolusion"></Input>
-                        </FormItem>
-                        <FormItem
-                            v-for="(item, index) in form.notice.tips"
-                            v-if="item.status"
-                            :key="index"
-                            :label="'规则 ' + item.index"
-                            :prop="'notice.tips.' + index + '.text'"
-                            :rules="{required: true, message: '规则 ' + item.index +' 不能为空', trigger: 'blur'}">
-                            <Row>
-                                <Col span="18">
-                                    <Input type="textarea" v-model="item.text" placeholder="Enter something..."></Input>
-                                </Col>
-                                <Col span="4" offset="1">
-                                    <Button type="ghost" @click="handleRemove(index)">Delete</Button>
-                                </Col>
+                <Card>
+                    <Row :gutter="10"  class="image-editor">
+                        <Col span="14" class="image-editor-con1">
+                            <div class="cropper">
+                                <img id="cropimg2" alt="">
+                            </div>
+                        </Col>
+                        <Col span="10" class="image-editor-con1">
+                            <Row type="flex" justify="center" align="middle" class="image-editor-con1-preview-con">
+                                <div id="iPreview2"></div>
                             </Row>
-                        </FormItem>
-                        <FormItem>
-                            <Row>
-                                <Col span="12">
-                                    <Button type="dashed" long @click="handleAdd" icon="plus-round">Add item</Button>
-                                </Col>
-                            </Row>
-                        </FormItem>
-                    </Card>
-                </Form>
-                
+                            <div class="image-editor-con1-btn-con margin-top-10">
+                                <input type="file" accept="image/png, image/jpeg, image/gif, image/jpg" @change="handleFileChange" id="fileinput1" class="fileinput" />
+                                <label class="filelabel" for="fileinput1"><Icon type="image"></Icon>&nbsp;选择图片</label>
+                                <span><Button @click="handlecrop" type="primary" icon="crop">确定</Button></span>
+                            </div>
+                            <Modal v-model="option.showCropedImage">
+                                <p slot="header">预览裁剪之后的图片</p>
+                                <img :src="option.cropedImg" alt="" v-if="option.showCropedImage" style="width: 100%;">
+                            </Modal>
+                        </Col>
+                    </Row>
+                </Card>
             </Col>
             <Col span="12"> 
-                <Card class="attach">
-                    <Upload action="/" accept="image/png, image/jpeg, image/gif, image/jpg"  :format="['jpg','jpeg','png']" :before-upload="handleAlbumUpload">
-                        <span>相册展示&nbsp;&nbsp;</span>
-                        <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button>
-                    </Upload>
-                    <template v-for="file in albumFiles">
-                        <div :key="file.name" class="attach-item">
-                            <Tag type="dot" closable @on-close="handleAlbumDelete" :name="file.name">{{file.name}}</Tag>
-                        </div>
-                    </template>
-                    <Upload action="/" accept="image/png, image/jpeg, image/gif, image/jpg" :format="['jpg','jpeg','png']" :before-upload="handleDeployUpload">
-                        <span>硬件配置&nbsp;&nbsp;</span>
-                        <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button>
-                    </Upload>
-                    <template v-for="file in deployFiles">
-                        <div :key="file.name" class="attach-item">
-                            <Tag type="dot" closable @on-close="handleDeployDelete" :name="file.name">{{file.name}}</Tag>
-                        </div>
-                    </template>
+                <Card>
+                    <h2>文本编辑</h2>
+                    <Form ref="formCustom" :model="form" :rules="ruleCustom" :label-width="80">
+                        <FormItem label="空间介绍" prop="mainInfo">
+                            <Input type="textarea" :rows="4" v-model="form.mainInfo"></Input>
+                        </FormItem>
+                        <FormItem
+                            v-for="(item, index) in form.menu"
+                            :key="index"
+                            :label="'Item ' + item.index"
+                            :prop="'menu.' + index + '.text'"
+                            :rules="{required: true, message: 'Item ' + item.index +' can not be empty', trigger: 'blur'}">
+                            <Row>
+                                <Input type="text" v-model="item.title" placeholder="标题..."></Input>
+                                <Input type="textarea" v-model="item.text" placeholder="简要介绍..."></Input>
+                            </Row>
+                        </FormItem>
+                    </Form>
                 </Card>    
             </Col>
             <Button type="ghost" class="submit" @click="handleSubmit('formCustom')">提交</Button>
@@ -77,146 +56,124 @@
 </template>
 <script>
 import util from '../../../../../libs/util.js';
+import Cropper from 'cropperjs';
+import '../cropper.min.css';
 export default {
-    name: 'homeInfo',
-    data () {
+        name: 'homeInfo',
+           data () {
         return {
-            currentType:'studyRoom',
-            spaceList:[
-                {
-                    label:'研习间',
-                    value:'studyRoom'
-                },
-                {
-                    label:'座位',
-                    value:'seat'
-                },
-            ],
-            albumFiles:[],
-            deployFiles:[],
-            index: 1,
+            cropper: {},
+            option: {
+                showCropedImage: false,
+                cropedImg: '',
+                imgName:'',
+            },
+            files:[],
             form:{
-                introduce:'',
-                notice:{
-                    tips:[{
-                            text: '',
-                            index: 1,
-                            status: 1
-                    }],
+                mainInfo:'',
+                menu:[{
                     title:'',
-                    prolusion:''
-                },
+                    text:'',
+                    index:1
+                },{
+                    title:'',
+                    text:'',
+                    index:2
+                },{
+                    title:'',
+                    text:'',
+                    index:3
+                }]
             },
             ruleCustom:{
-                introduce:[{
+                noticeTitle:[{
                     //validator: validateCard, trigger: 'blur' 
                 }],
-                notice:[{
+                noticeText:[{
+                    //validator: validateName, trigger: 'blur' 
+                }],
+                publisher:[{
                     //validator: validateName, trigger: 'blur' 
                 }],
             }
         };
     },
     methods:{
-        handleAlbumUpload(file){
-            for(let i=0;i<this.albumFiles.length;i++){
-                let item=this.albumFiles[i];
-                if(item.name==file.name){
-                    this.$Message.warning({
-                        content: '此文件已存在上传列表中'
-                    });
-                    return false;
-                }
-            }
-            this.albumFiles.push(file);
-            return false;
+        handleFileChange (e) {
+            let file = e.target.files[0];
+            this.option.imgName=file.name.split('.')[0];
+            let reader = new FileReader();
+            reader.onload = () => {
+                this.cropper.replace(reader.result);
+                reader.onload = null;
+            };
+            reader.readAsDataURL(file);
         },
-        handleDeployUpload(file){
-             for(let i=0;i<this.deployFiles.length;i++){
-                let item=this.deployFiles[i];
-                if(item.name==file.name){
-                    this.$Message.warning({
-                        content: '此文件已存在上传列表中'
-                    });
-                    return false;
-                }
-            }
-            this.deployFiles.push(file);
-            return false;
-        },
-        handleAlbumDelete(event,name){
-            for(let i=0;i<this.albumFiles.length;i++){
-                let item=this.albumFiles[i];
-                if(item.name==name){
-                    this.albumFiles.splice(i,1);
-                    break;
-                }
-            }
-        },
-        handleDeployDelete(event,name){
-            for(let i=0;i<this.deployFiles.length;i++){
-                let item=this.deployFiles[i];
-                if(item.name==name){
-                    this.deployFiles.splice(i,1);
-                    break;
-                }
-            }
-        },
-        handleAdd () {
-            this.index++;
-            this.form.notice.tips.push({
-                text: '',
-                index: this.index,
-                status: 1
-            });
-        },
-        handleRemove (index) {
-            this.form.notice.tips[index].status = 0;
-        },
-        handleSelectChange(){
-
+        handlecrop () {
+            let file = this.cropper.getCroppedCanvas().toDataURL();
+            this.option.cropedImg = file;
+            this.option.showCropedImage = true;
         },
         handleSubmit(name){
             let that=this;
             this.$refs[name].validate((valid) => {
                 if (valid) {
                     let form=new FormData();
-                    form.append("introduce",this.form.introduce);
-                    let notice={};
-                    notice.title=this.form.notice.title;
-                    notice.prolusion=this.form.notice.prolusion;
-                    notice.tips=[];
-                    for(let item of this.form.notice.tips){
-                        if(item.status==1){
-                            let temp={};
-                            temp.text=item.text;
-                            notice.tips.push(temp);
-                        }
+                    form.append("mainInfo",this.form.mainInfo);
+                    let menu=[];
+                    for(let item of this.form.menu){
+                        let temp={};
+                        temp.title=item.title;
+                        temp.text=item.text;
+                        menu.push(temp);
                     }
-                    form.append("notice",JSON.stringify(notice));
-                    if(this.albumFiles.length){
-                        form.append('albumFiles',this.albumFiles);   
-                    }  
-                    if(this.deployFiles.length){
-                        form.append('deployFiles',this.deployFiles);   
-                    }  
+                    form.append("menu",JSON.stringify(menu));
+                    if(this.option.cropedImg){
+                        let imgFile=this.base64ToBlob(this.option.cropedImg); 
+                        form.append('mainPic',imgFile,this.option.imgName+'.png');      
+                    }
                     that.$ajax
-                        .put(
-                            util.adminUrl+"/spaceNotice/?type="+this.currentType,
+                        .post(
+                            util.adminUrl+"/homePage/",
                             form,
                         ).then(function(response){
                             console.log(response.code);
                         })
-                    this.$Message.success('Success!');
-                } else {
-                    this.$Message.error('Fail!');
                 }
-            })
+            });
         },
+        base64ToBlob(urlData) {        //将base64图片转化为可以上传的file对象
+            var arr = urlData.split(',');
+            var mime = arr[0].match(/:(.*?);/)[1] || 'image/png';
+            // 去掉url的头，并转化为byte
+            var bytes = window.atob(arr[1]);
+            // 处理异常,将ascii码小于0的转换为大于0
+            var ab = new ArrayBuffer(bytes.length);
+            // 生成视图（直接针对内存）：8位无符号整数，长度1个字节
+            var ia = new Uint8Array(ab);
+            
+            for (var i = 0; i < bytes.length; i++) {
+                ia[i] = bytes.charCodeAt(i);
+            }
+            return new Blob([ab], {
+                type: mime
+            });
+        }
+    
     },
     created(){
     },
     mounted(){
+        let img = document.getElementById('cropimg2');
+        this.cropper = new Cropper(img, {
+            dragMode: 'move',
+            preview: '#iPreview2',
+            restore: false,
+            center: false,
+            highlight: false,
+            cropBoxMovable: false,
+            toggleDragModeOnDblclick: false
+        });
     }
 };
 </script>
